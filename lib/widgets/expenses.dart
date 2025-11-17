8c1dbe65238adbc6d4d7a80472549afa8c7c78ef
rakhisdeyudha <rakhisyuda@gmail.com>
2025-11-17 12:30:12 +0700

    adding chart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -92,10 +93,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          Text(
-            'The Chart',
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-          ),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
